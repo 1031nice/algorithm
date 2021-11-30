fa8ab6d9e7de61de350e61e1cd2776eaff00589a
Dong-Hun <1031nice@gmail.com>
2021-11-30 09:45:20 +0900

    [Leet Code] 45. Jump Game 2(RE) #Greedy #DP

--- src/leetcode/P45JumpGame2.java
+++ src/leetcode/P45JumpGame2.java
@@ -3,10 +3,38 @@ package leetcode;
 public class P45JumpGame2 {
     public int jump(int[] nums) {
         int[] cache = new int[nums.length];
+        return func(nums, cache, 0);
+    }
+
+    //----------------------------------------------------
+    // final solution: 각 칸에서 갈 수 있는 모든 칸을 검사할 필요가 없다
+    private int func(int[] nums, int[] cache, int now) {
+        if(now >= nums.length-1) {
+            return cache[now] = 0;
+        }
+
+        int maxIndex = 0;
+        int maxLen = 0;
+        for(int i=1; i<=nums[now]; i++) {
+            if(now + i >= nums.length-1) {
+                return cache[now] = 1;
+            }
+            int nextMaxLen = i + nums[now + i];
+            if(nextMaxLen > maxLen) {
+                maxLen = nextMaxLen;
+                maxIndex = now + i;
+            }
+        }
+        return cache[now] = func(nums, cache, maxIndex) + 1;
+    }
+
+    //----------------------------------------------------
+    // 개선 v1: 앞에서부터 시작하면 불필요한 재귀호출이 있으므로 뒤에서부터 출발하자
+    /*public int jump(int[] nums) {
+        int[] cache = new int[nums.length];
         func(nums, cache, nums.length-1);
         return cache[0];
     }
-
     private void func(int[] nums, int[] cache, int now) {
         int ret = 10_000;
         if(now >= nums.length-1) {
@@ -25,5 +53,5 @@ public class P45JumpGame2 {
         if(now != 0) {
             func(nums, cache, now-1);
         }
-    }
+    }*/
 }
