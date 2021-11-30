a24682520cce38d7edf94a3c40ca6cf68482857b
Dong-Hun <1031nice@gmail.com>
2021-11-30 10:14:40 +0900

    [Leet Code] 45. Jump Game 2(RE) Greedy 풀이에서 cache 제거

--- src/leetcode/P45JumpGame2.java
+++ src/leetcode/P45JumpGame2.java
@@ -2,34 +2,33 @@ package leetcode;
 
 public class P45JumpGame2 {
     public int jump(int[] nums) {
-        int[] cache = new int[nums.length];
-        return func(nums, cache, 0);
+        return func(nums, 0);
     }
 
     //----------------------------------------------------
-    // final solution: 각 칸에서 갈 수 있는 모든 칸을 검사할 필요가 없다
-    private int func(int[] nums, int[] cache, int now) {
+    // final solution Greedy(각 칸에서 갈 수 있는 모든 칸을 검사할 필요가 없다)
+    private int func(int[] nums, int now) {
         if(now >= nums.length-1) {
-            return cache[now] = 0;
+            return 0;
+        }
+        else if(now + nums[now] >= nums.length-1) {
+            return 1;
         }
 
-        int maxIndex = 0;
         int maxLen = 0;
+        int maxIdx = 0;
         for(int i=1; i<=nums[now]; i++) {
-            if(now + i >= nums.length-1) {
-                return cache[now] = 1;
-            }
-            int nextMaxLen = i + nums[now + i];
-            if(nextMaxLen > maxLen) {
-                maxLen = nextMaxLen;
-                maxIndex = now + i;
+            int nowMaxLen = nums[now + i] + i;
+            if(nowMaxLen > maxLen) {
+                maxLen = nowMaxLen;
+                maxIdx = now + i;
             }
         }
-        return cache[now] = func(nums, cache, maxIndex) + 1;
+        return func(nums, maxIdx) + 1;
     }
 
     //----------------------------------------------------
-    // 개선 v1: 앞에서부터 시작하면 불필요한 재귀호출이 있으므로 뒤에서부터 출발하자
+    // DP 풀이 && 앞에서부터 시작하면 불필요한 재귀호출이 있으므로 뒤에서부터 출발하자
     /*public int jump(int[] nums) {
         int[] cache = new int[nums.length];
         func(nums, cache, nums.length-1);
