db61b482961f8f8f973336afa3081b058b2ee5b9
Dong-Hun Gang <1031nice@gmail.com>
2022-12-26 21:03:28 +0900

    [백준] 5430. AC(G5)

---
+++ src/boj/gold/P5430AC.kt
@@ -0,0 +1,40 @@
+package boj.gold
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    repeat(nextInt()) {
+        val commands = next()
+        val length = nextInt()
+        nextLine() // clear buffer
+        val tempArray = nextLine()
+        val array = tempArray.substring(1, tempArray.lastIndex).split(",")
+
+        var isFront = true
+        var start = 0
+        var end = length
+
+        for (ch in commands) {
+            if (ch == 'R') {
+                isFront = isFront xor true
+            } else {
+                if (isFront) {
+                    start++
+                } else {
+                    end--
+                }
+            }
+        }
+
+        if (start > end) {
+            println("error")
+            return@repeat
+        }
+
+        var answer = array.subList(start, end)
+        if (!isFront) {
+            answer = answer.reversed()
+        }
+        println(answer.joinToString(",", "[", "]"))
+    }
+}
