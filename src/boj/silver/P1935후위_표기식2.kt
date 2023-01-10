38655c9082cf8352b037eae8ef258f640be66ce3
donghun <1031nice@gmail.com>
2023-01-10 17:10:56 +0900

    [백준] 1935. 후위 표기식2(S3) #Stack

---
+++ src/boj/silver/P1935후위_표기식2.kt
@@ -0,0 +1,32 @@
+package boj.silver
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val operands = nextInt()
+    val expression = next()
+    val values = Array(26) { 0 }
+
+    repeat(operands) {
+        values[it] = nextInt()
+    }
+
+    val stack = Stack<Double>()
+
+    for (ch in expression) {
+        if (ch.isLetter()) {
+            stack.push(values[ch - 'A'].toDouble())
+        } else {
+            val operand2 = stack.pop()
+            val operand1 = stack.pop()
+            when (ch) {
+                '+' -> stack.push(operand1 + operand2)
+                '-' -> stack.push(operand1 - operand2)
+                '*' -> stack.push(operand1 * operand2)
+                '/' -> stack.push(operand1 / operand2)
+            }
+        }
+    }
+
+    println(String.format("%.2f", stack.pop()))
+}
